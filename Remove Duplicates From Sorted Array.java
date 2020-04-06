55610104dcc6ea86136ec1bde11293385d4cd1ff
ylyu4 <57893326+ylyu4@users.noreply.github.com>
2020-04-05 20:14:20 -0400

    Add files via upload

---
+++ Remove Duplicates From Sorted Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        
+        int len = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[len] != nums[i]){
+                len++;
+                nums[len] = nums[i];
+            }    
+        }
+        
+        return len + 1;
+    }
+    
+}
